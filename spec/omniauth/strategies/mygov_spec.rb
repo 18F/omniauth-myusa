a6e035a20b819d68c4e5142c80e4bb8004a672e4
Greg Gershman <greg@shelrick.com>
2012-10-23 16:57:06 -0400

    Add really, really basic specs.

--- spec/omniauth/strategies/mygov_spec.rb
+++ spec/omniauth/strategies/mygov_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
 describe OmniAuth::Strategies::Mygov do
-  it 'should do some testing' do
-    pending
+  subject do
+    OmniAuth::Strategies::Mygov.new({})
+  end
+
+  context "client options" do
+    it 'should have correct name' do
+      subject.options.name.should eq("mygov")
+    end
+
+    it 'should have correct site' do
+      subject.options.client_options.site.should eq('https://my.usa.gov')
+    end
+
+    it 'should have correct token url' do
+      subject.options.client_options.token_url.should eq('/oauth/authorize')
+    end
   end
 end
