e7ee874b61dfae6fb3a673cecf35553bb3decb55
Greg Gershman <greg@shelrick.com>
2012-10-23 16:51:31 -0400

    Modifications to the MyGov strategy to get it to work properly.

--- lib/omniauth/strategies/mygov.rb
+++ lib/omniauth/strategies/mygov.rb
@@ -4,7 +4,7 @@ module OmniAuth
   module Strategies
     class Mygov < OmniAuth::Strategies::OAuth2
       # Give your strategy a name.
-      option :name, "MyGov"
+      option :name, "mygov"
 
       # This is where you pass the options you would pass when
       # initializing your consumer from the OAuth gem.
@@ -15,23 +15,23 @@ module OmniAuth
       # additional calls (if the user id is returned with the token
       # or as a URI parameter). This may not be possible with all
       # providers.
-      uid{ raw_info['id'] }
+      uid{ raw_info['user']['id'] }
 
       info do
         {
-          :name => raw_info['name'],
-          :email => raw_info['email']
+          :name => raw_info['user']['name'],
+          :email => raw_info['user']['email']
         }
       end
 
       extra do
         {
-          'raw_info' => raw_info
+          'raw_info' => raw_info['user']
         }
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/profile').parsed
+        @raw_info ||= access_token.get('/api/profile').parsed
       end
     end
   end
